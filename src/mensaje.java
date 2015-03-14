017c0e2011a9314a8ae4b5e598cfc5e727a6f100
CRISJC <crisjc92@gmail.com>
2015-03-14 12:15:14 -0500

    Se agrego autor al proyecto (2)

--- src/mensaje.java
+++ src/mensaje.java
@@ -1,15 +1,10 @@
 
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Cristan Pinta
+ * @author Cristan Pinta y Cristian Gaona
  */
 public class mensaje {public static void main(String[] args) {
             JOptionPane.showMessageDialog(null, "Bienvenido a java");
